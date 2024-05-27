e755f7764284c53d6ea1ec5a6df0ec36e0648df6
doandung <doantrungdung2001@gmail.com>
2024-05-27 16:00:29 +0700

    update infor user in home page

--- App/Components/HeaderComponents/HeaderComponents.jsx
+++ App/Components/HeaderComponents/HeaderComponents.jsx
@@ -1,32 +1,63 @@
-import { Text, View, Image, TouchableOpacity, TextInput } from "react-native";
-import React from "react";
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  ActivityIndicator,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import UserInfoAsyncStorage from "../../Utils/UserInfoAsyncStorage";
+import useClientInformation from "../../Screens/ProfileUserScreen/ProfileInformation/useClientInformation";
 import styles from "./Header.Styles";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { useNavigation } from "@react-navigation/native";
+import { formatDate } from "../../Utils/helper";
 const HeaderComponents = () => {
+  const [userId, setUserId] = useState(null);
   const navigation = useNavigation();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await UserInfoAsyncStorage.getUserInfo("UserInfo");
+        setUserId(result.farm._id);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  const { dataClient, isSuccessClientInformation, isLoadingClientInformation } =
+    useClientInformation({
+      clientId: userId,
+    });
+
   return (
     <View style={styles.container}>
-      <View style={styles.card}>
-        <View style={styles.header}>
-          <Text style={styles.headerTitle}>Hà Nội- Hai Bà Trưng</Text>
-          <Text style={styles.headerSubtitle}>24 March, 18pm - 19pm</Text>
-        </View>
+      {isSuccessClientInformation && (
+        <View style={styles.card}>
+          <View style={styles.header}>
+            <Text style={styles.headerTitle}>{dataClient.address}</Text>
+            <Text style={styles.headerSubtitle}>{formatDate(new Date())}</Text>
+          </View>
 
-        <View style={styles.body}>
-          <Image
-            source={{
-              uri: "https://bootdey.com/img/Content/avatar/avatar6.png",
-            }}
-            style={styles.avatar}
-          />
-          <View style={styles.userInfo}>
-            <Text style={styles.userName}>Mr. Cody Fisher</Text>
-            <Text style={styles.userRole}>Professor</Text>
+          <View style={styles.body}>
+            <Image
+              source={{
+                uri: "https://bootdey.com/img/Content/avatar/avatar6.png",
+              }}
+              style={styles.avatar}
+            />
+            <View style={styles.userInfo}>
+              <Text style={styles.userName}>{dataClient.name}</Text>
+              <Text style={styles.userRole}>{dataClient.email}</Text>
+            </View>
           </View>
         </View>
-      </View>
-
+      )}
+      {isLoadingClientInformation && (
+        <ActivityIndicator size="large" color="#00ff00" />
+      )}
       <View style={styles.titleSearch}>
         <Text style={styles.headingText}>Nông trại bạn muốn tìm gì?</Text>
       </View>
